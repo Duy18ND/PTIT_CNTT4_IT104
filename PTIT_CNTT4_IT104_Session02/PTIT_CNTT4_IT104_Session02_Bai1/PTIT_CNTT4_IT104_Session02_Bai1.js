8be9c6a94e559bec392ef63fee93955de0d0ca3f
Duy <duy18namdinh@gmail.com>
2025-08-04 22:49:13 +0700

    ss02

--- PTIT_CNTT4_IT104_Session02/PTIT_CNTT4_IT104_Session02_Bai1/PTIT_CNTT4_IT104_Session02_Bai1.js
+++ PTIT_CNTT4_IT104_Session02/PTIT_CNTT4_IT104_Session02_Bai1/PTIT_CNTT4_IT104_Session02_Bai1.js
@@ -1,10 +1,10 @@
 const sum = (arr) => {
     let sum = 0;
     for (const i of arr) {
-        if(arr[i] % 2 == 0){
-            sum += arr[i];
+        if (i % 2 === 0) {
+            sum += i;
         }
     }
     console.log(sum);
 };
-sum([1,2,4,5,6]);
+sum([1, 2, 4, 5, 6]);
